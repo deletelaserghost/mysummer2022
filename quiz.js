9a8a0945174852db72982367a2c69564a8e8c52b
DeleteLaserGhost <wedidntevenlearnthis@gmail.com>
2025-11-13 20:19:56 -0800

    Update quiz.js

--- quiz.js
+++ quiz.js
@@ -1,20 +1,15 @@
-$("#prospects_form").submit(function(e) {
-  e.preventDefault();
-});
-let value = 0;
-function calcGrade() {
-  leastfavoriteride = parseInt(document.querySelector('input[name = "leastfavoriteride"]:checked').value);
-  dcfear = parseInt(document.querySelector('input[name = "dcfear"]:checked').value);
-  lastdayschool = parseInt(document.querySelector('input[name = "lastdayschool"]:checked').value);
-  ushdate = parseInt(document.querySelector('input[name = "ushdate"]:checked').value);
-  dcpeople = parseInt(document.querySelector('input[name = "dcpeople"]:checked').value);
-  numberofhotels = parseInt(document.querySelector('input[name = "numberofhotels"]:checked').value);
-  lastactivity = parseInt(document.querySelector('input[name = "lastactivity"]:checked').value);
-  daynoattend = parseInt(document.querySelector('input[name = "daynoattend"]:checked').value);
-  websitefor = parseInt(document.querySelector('input[name = "websitefor"]:checked').value);
-  buyicecream = parseInt(document.querySelector('input[name = "buyicecream"]:checked').value);
-  result = leastfavoriteride + dcfear + lastdayschool + ushdate + dcpeople + numberofhotels + lastactivity + daynoattend + websitefor + buyicecream;
-  document.getElementById("grade").innerHTML = result;
-  return;
+var answers = ["B", "C", "C", "B", "A", "B", "D", "D", "C", "D"];
+function getValues(radioName) {
+var radios = document.getElementsByName(radioName);
+for (var y = 0; y < radios.length; y++)
+  if (radios[y].checked) return radios[y].value;
 }
-
+function calcGrade() {
+var grade = 0;
+for (var i = 0; i < 10; i++)
+  if (getValues("question" + i) === answers[i]) grade +=1;
+return grade;
+}
+function returnGrade() {
+  document.getElementById("grade").innerHTML = calcGrade();
+}
